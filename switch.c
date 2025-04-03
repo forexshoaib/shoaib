bc508d730dec67b198cf20ab9f060791b5e4034b
forexshoaib <shoaib.malik101@icloud.com>
2025-04-03 15:43:10 +0530

    file added

---
+++ switch.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int case = 2;
+	switch(case){
+		case 1:
+			printf("aiml");
+		case 2:
+			printf("cse");
+		case 3:
+			printf("3");
+		case 4:
+			printf("4");
+	}
